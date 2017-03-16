0bd0833a9cc48ad244cc6bb5cdda29b7215a39fc
Xuan Nguyen Duc <xuan.nguyenduc@mbp-0003.local>
2017-03-16 22:14:22 +0700

    add logging

--- JKMoviesServer/app.js
+++ JKMoviesServer/app.js
@@ -8,28 +8,63 @@ const configureRoutes = require('./src/routes/movies.route');
 const server = new Hapi.Server();
 server.connection({
 	host: 'localhost',
-  port: 3001
+	port: 3001
 });
+
 const options = {
 	info: {
 		'title': 'Test API Documentation',
 	}
 };
 
+const eventOptions = process.env.NODE_ENV === 'production'?
+						{
+							error: '*'
+						}:
+						{
+							log: '*',
+							request: '*',
+							response: '*'
+						};
+const goodOptions = {
+    reporters: {
+		myConsoleReporter: [
+			{
+				module: 'good-squeeze',
+				name: 'Squeeze',
+				args: [eventOptions]
+        	},
+			{
+				module: 'good-console'
+        	},
+			'stdout'
+			],
+    }
+};
+
 server.register([
 	Inert,
-  Vision,
-  {
+  	Vision,
+  	{
 		'register': HapiSwagger,
-		'options': options
-  }], (err) => {
+		'options': {
+						info: {
+							'title': 'Test API Documentation',
+						}
+					}
+  	},
+	{
+		'register': require('good'),
+		'options': goodOptions
+	}
+	], (err) => {
 		server.start( (err) => {
-    if (err) {
-			console.log(err);
-    } else {
-			console.log('Server running at:', server.info.uri);
-		}
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('Server running at:', server.info.uri);
+			}
+		});
 	});
-});
 
 server.route(configureRoutes());
