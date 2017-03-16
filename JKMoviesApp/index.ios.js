6bef5dfc1e4c909c040a7bc78a525dc4d8e5352b
Hien Phan The <hien.phanthe@pycogroup.com>
2017-03-16 17:06:47 +0700

    finished setup react-native, starting apply redux

---
+++ JKMoviesApp/index.ios.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry
+} from 'react-native';
+
+import App from './src/App'
+
+export default class JKMoviesApp extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+
+
+AppRegistry.registerComponent('JKMoviesApp', () => JKMoviesApp);
