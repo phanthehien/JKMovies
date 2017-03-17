2f565febada35fdebfa06ba850bf3f5e18039dfa
Hien Phan The <hien.phanthe@pycogroup.com>
2017-03-17 12:16:02 +0700

    add movies list and movies items

--- JKMoviesApp/src/components/MovieItem.component.js
+++ JKMoviesApp/src/components/MovieItem.component.js
@@ -7,8 +7,34 @@ import React, {Component} from 'react';
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Image
 } from 'react-native';
+
+
+const styles = StyleSheet.create ({
+    image  : {
+        width: 50, height: 50
+    },
+
+    title :{
+        fontWeight: 'bold',
+        color:'blue'
+    },
+    item: {
+        width: "100%",
+        margin: 0,
+        marginTop: -1,
+        padding: 15,
+        paddingTop: 30,
+        paddingBottom: 30,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: 'green',
+        borderWidth: 1
+    }
+});
 
 
 class MovieItem extends Component {
@@ -19,14 +45,11 @@ class MovieItem extends Component {
         var { title, type, director, length, short_description, photo_url } = movie;
 
         return (
-            <View>
+            <View style={styles.item}>
+                <Image style={styles.image} source={{uri: photo_url}} />
+                <Text style={styles.title} >{title}</Text>
                 <Text>
-                    {title}
                     {type}
-                    {director}
-                    {length}
-                    {short_description}
-                    {photo_url}
                 </Text>
             </View>
         );
