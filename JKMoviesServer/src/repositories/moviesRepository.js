b187162e7d956c7ab26e683d0fa5cafdc33ac598
Xuan Nguyen Duc <xuan.nguyenduc@mbp-0003.local>
2017-03-17 11:26:35 +0700

    load movies from db

--- JKMoviesServer/src/repositories/moviesRepository.js
+++ JKMoviesServer/src/repositories/moviesRepository.js
@@ -1,22 +1,5 @@
+const Movie = require('./movie');
+
 module.exports.loadMovies = () => {
-	return new Promise((resolve, reject) => {
-		resolve([
-			{
-				title:"Flight of Fury",
-				type: "Horror",
-				director: "Xuan Nguyen",
-				length: 123,
-				short_description:"This is a cool film",
-				photo_url: "http://movie.com/photo/fligh.png"
-			},
-			{
-				title:"Flight of Fury 22",
-				type: "Horror 22",
-				director: "Xuan Nguyen 22",
-				length: 123,
-				short_description:"This is a cool film  222",
-				photo_url: "http://movie.com/photo/fligh.png"
-			}
-		])
-	})
+	return Movie.find({}).exec();
 }
